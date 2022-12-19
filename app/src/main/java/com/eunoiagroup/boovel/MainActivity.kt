1b264d368e55e0fcb4bd1b22c02360f6ac99c57e
MuhammadIqbalK <muhammad.iqbal.krisna-2020@vokasi.unair.ac.id>
2022-12-19 21:11:02 +0700

    commit first

---
+++ app/src/main/java/com/eunoiagroup/boovel/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.eunoiagroup.boovel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
