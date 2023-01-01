95c394603b685f65543e18941ad4e56044bac17b
Thariqi Ruli <thariqiruli01@gmail.com>
2023-01-02 03:20:33 +0700

    commit toolbar pada fragment tiket dan activity pencarian

--- app/src/main/java/com/eunoiagroup/boovel/PencarianActivity.kt
+++ app/src/main/java/com/eunoiagroup/boovel/PencarianActivity.kt
@@ -2,13 +2,13 @@ package com.eunoiagroup.boovel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
-class BerandaActivity : AppCompatActivity() {
+class PencarianActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_beranda)
+        setContentView(R.layout.activity_pencarian)
 
     }
-
 
 }
