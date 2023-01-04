dac80417752ce37db952c484473665ad781af319
Thariqi Ruli <thariqiruli01@gmail.com>
2023-01-04 21:07:00 +0700

    commit toolbar hail pencarian

--- app/src/main/java/com/eunoiagroup/boovel/PencarianActivity.kt
+++ app/src/main/java/com/eunoiagroup/boovel/PencarianActivity.kt
@@ -3,12 +3,12 @@ package com.eunoiagroup.boovel
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.widget.Button
 
 class PencarianActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pencarian)
-
     }
 
 }
